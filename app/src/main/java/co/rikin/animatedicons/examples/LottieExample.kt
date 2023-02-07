61e90441d2ab15c18c3f4f954d8d74fd63b59b10
Rikin Marfatia <rikinm10@gmail.com>
2023-02-06 19:01:55 -0800

    More Lottie Scaffolding

--- app/src/main/java/co/rikin/animatedicons/examples/LottieExample.kt
+++ app/src/main/java/co/rikin/animatedicons/examples/LottieExample.kt
@@ -1,10 +1,40 @@
 package co.rikin.animatedicons.examples
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.rikin.animatedicons.R
+import co.rikin.animatedicons.ui.theme.TerraCotta
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieAnimatable
+import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlinx.coroutines.launch
 
 @Preview
 @Composable
 fun LottieIcon() {
-
+  Box(
+    modifier = Modifier
+      .size(60.dp)
+      .background(
+        color = TerraCotta,
+        shape = RoundedCornerShape(16.dp)
+      )
+      .clip(RoundedCornerShape(16.dp))
+      .clickable {
+      },
+    contentAlignment = Alignment.Center
+  ) {
+  }
 }
