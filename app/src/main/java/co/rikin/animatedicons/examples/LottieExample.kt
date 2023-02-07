fc40967c3500cd4d92c631f82baa0bc49c30d520
Rikin Marfatia <rikinm10@gmail.com>
2023-02-06 18:49:21 -0800

    Lottie Example Scaffold

--- app/src/main/java/co/rikin/animatedicons/examples/LottieExample.kt
+++ app/src/main/java/co/rikin/animatedicons/examples/LottieExample.kt
@@ -1,2 +1,10 @@
 package co.rikin.animatedicons.examples
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun LottieIcon() {
+
+}
